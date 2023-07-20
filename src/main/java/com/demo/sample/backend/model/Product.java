5ee5f05da4d5497bea8640b6bc59aa7e659c6ad0
vigneszwar <vignezwar@gmail.com>
2023-07-20 20:27:33 +0530

    Product Backend Service with h2 in memory database

--- src/main/java/com/demo/sample/backend/model/Product.java
+++ src/main/java/com/demo/sample/backend/model/Product.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
     private String name;
     private String description;
