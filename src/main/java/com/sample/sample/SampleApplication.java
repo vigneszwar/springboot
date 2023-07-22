5a7d3e9ddc57d0d1d9eb711a4f5803011bbc1173
sanjay733 <sanjaynevamind@gmail.com>
2023-07-22 15:27:25 +0530

    i did a commit

--- src/main/java/com/sample/sample/SampleApplication.java
+++ src/main/java/com/sample/sample/SampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
-
+//committed
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
