6bdbc89a759203dd25d2cbd20b4e1bfb2fa3b121
sandh <sandhanamvimal0723@gmail.com>
2023-07-22 15:29:21 +0530

    Push

--- src/main/java/com/sample/sample/GreetClass.java
+++ src/main/java/com/sample/sample/GreetClass.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 
 // component for greet class
-//This class is for componenet name greetclass
+//This class is for componenet named greetclass
 @Component
 public class GreetClass {
 
