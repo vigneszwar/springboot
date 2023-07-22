3168e43467b9ffae5be5afbec5118a9afefbd57f
GIRISH <naveenutthra27@gmail.com>
2023-07-22 15:24:19 +0530

    comitted

--- src/main/java/com/sample/sample/SampleApplication.java
+++ src/main/java/com/sample/sample/SampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
-
+//commented over here
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
