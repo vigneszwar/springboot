4fee206d881a63729394eb6cc033ce13f7c27ae9
sarath <psarath290902@gmail.com>
2023-07-22 15:30:43 +0530

    list object

--- src/main/java/com/sample/sample/ConfigurationClass.java
+++ src/main/java/com/sample/sample/ConfigurationClass.java
@@ -10,10 +10,10 @@ public class ConfigurationClass {
     @Bean
     public ArrayList<Integer> getNumbers() {
         System.out.println("Creating ArrayList Numbers object");
-        ArrayList<Integer> objects = new ArrayList<>();
-        objects.add(1);
-        objects.add(2);
-        objects.add(4);
-        return objects;
+        ArrayList<Integer> object = new ArrayList<>();
+        object.add(1);
+        object.add(2);
+        object.add(4);
+        return object;
     }
 }
