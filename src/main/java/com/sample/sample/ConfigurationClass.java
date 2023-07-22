97deb9314285becf961223406b1ca8480cc0fae8
Paramesh <parrameshwaran1430@gmail.com>
2023-07-22 15:29:10 +0530

    comment

--- src/main/java/com/sample/sample/ConfigurationClass.java
+++ src/main/java/com/sample/sample/ConfigurationClass.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 @Configuration
 public class ConfigurationClass {
     @Bean
-    // get numbers for change
+    // get number is function
     public ArrayList<Integer> getNumbers() {
         System.out.println("Creating ArrayList Numbers object");
         ArrayList<Integer> objects = new ArrayList<>();
